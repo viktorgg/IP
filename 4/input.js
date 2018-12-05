f3812ec58031618a7e37deebc30d595057ad26ee
viktorgg <viktor.g.g@abv.bg>
2018-12-05 11:37:31 +0200

    Homework 4

---
+++ 4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z]+[a-z]+/,
+"exercise_2": /088[^0-]{7}/,
+"exercise_3": /[^0-1]+/,
+"exercise_4": /^[^\._0-9][a-zA-Z0-9\._]{2,13}$/,
+"exercise_5": /[1|9][1-9][1|9][1-9]?/,
+"exercise_6": /class=['|"][a-zA-Z\s]*['|"]/
+};
+cssSelectors = {
+"exercise_1": "css > item > java",
+"exercise_2": "different > java",
+"exercise_3": "css > item > java > tag",
+"exercise_4": "css > item:nth-of-type(3)",
+"exercise_5": "css > item > tag > java:nth-last-of-type(2)",
+"exercise_6": "css > item > item > item > item > item",
+"exercise_7": "different:nth-of-type(2) > java:nth-of-type(2)",
+"exercise_8": "css > #someId"
+};
